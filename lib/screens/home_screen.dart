cbc877dc80955d35297602dbea4864b79bb979ed
Jose Rios <jrbernabe@gmail.com>
2023-10-10 16:35:20 -0400

    Add initial home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app_flutter/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Movie listings'),
+        elevation: 0,
+        actions: const [
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.search_outlined),
+          ),
+        ],
+      ),
+      body: const Column(
+        children: [
+          CardSwiper(),
+        ],
       ),
     );
   }
