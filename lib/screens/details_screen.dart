95d7b06e21f8058afe5b314a75a5dace0e6a215e
Jose Rios <jrbernabe@gmail.com>
2023-10-11 12:21:03 -0400

    Add initial details screen with cards

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,14 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app_flutter/themes/main_theme.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Details Screen'),
+    //TODO: Change by a movie instance
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle(),
+            ]),
+          )
+        ],
       ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: MainTheme.primary,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/images/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: const FadeInImage(
+            placeholder: AssetImage('assets/images/no_image.jpg'),
+            image: NetworkImage('https://via.placeholder.com/200x300'),
+            height: 150,
+          ),
+        ),
+        const SizedBox(width: 20),
+        Column(
+          children: [
+            Text(
+              'movie.title',
+              style: textTheme.titleLarge,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              'movie.originalTitle',
+              style: textTheme.titleMedium,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Row(
+              children: [
+                const Icon(
+                  Icons.star_outline,
+                  size: 15,
+                  color: Colors.grey,
+                ),
+                const SizedBox(width: 5),
+                Text(
+                  'movie.voteAverage',
+                  style: textTheme.bodySmall,
+                ),
+              ],
+            )
+          ],
+        )
+      ]),
     );
   }
 }
