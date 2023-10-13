f09964d82aae31318610aeae477c7c6699b2d7ea
Jose Rios <jrbernabe@gmail.com>
2023-10-13 15:49:25 -0400

    Config environment variables with dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movies_app_flutter/screens/screens.dart';
 import 'package:movies_app_flutter/themes/main_theme.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
