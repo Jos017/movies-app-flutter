305328b397e4622c33743647c96329796e2f4e7d
Jose Rios <jrbernabe@gmail.com>
2023-10-13 16:19:32 -0400

    Config movie provider for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movies_app_flutter/providers/movies_provider.dart';
 import 'package:movies_app_flutter/screens/screens.dart';
 import 'package:movies_app_flutter/themes/main_theme.dart';
+import 'package:provider/provider.dart';
 
 Future main() async {
   await dotenv.load(fileName: '.env');
-  runApp(const MyApp());
+  runApp(const AppState());
+}
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MoviesProvider(), lazy: false),
+      ],
+      child: const MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
